192ff7f4ab47c0ab4da64f03d0c65adbaf7309a6
maulanabin <2041720132@student.polinema.ac.id>
2022-12-20 11:16:43 +0700

    Commit to GitHub

---
+++ README.md
@@ -0,0 +1,12 @@
+
+===
+
+SISTEM INFORMASI  PENJUALAN PADA BILLIE BEANS SUPPLY COFFE.CO BERBASIS WEB
+
+### TI 3H
+
+| No |         Nama        |     NIM    |
+|:--:|:-------------------:|:----------:|
+| 13 | Maulana Bintang I.  | 2041720132 |
+
+===
---
+++ app/Http/Controllers/ReceiptController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProductTransaction;
+use App\Models\Receipt;
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ReceiptController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Receipt  $receipt
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Receipt $receipt)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Receipt  $receipt
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Receipt $receipt)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Receipt  $receipt
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Receipt $receipt)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Receipt  $receipt
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Receipt $receipt)
+    {
+        //
+    }
+
+    public function infoInvoice($id)
+    {
+        $transaksi = Transaction::where('id_customer',Auth::user()->id)->where('id_transaksi',$id)->first();
+        $produktransaksi = ProductTransaction::with('produk','produktransaksi')->where('id_transaksi',$id)->get();
+        $subtotal = 0;
+        foreach($produktransaksi as $pt){
+            $subtotal = $subtotal + ($pt->produk->harga * $pt->jumlah);
+        }
+        return view('invoice.index', compact('transaksi', 'produktransaksi', 'subtotal'));
+    }
+}
